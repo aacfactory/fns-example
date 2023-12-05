2b65b4e40484ae564954ff0143ed639516579e4c
wangminxiang <ryougi.nevermore@hotmail.com>
2023-12-06 02:44:39 +0800

    mod

--- standalone/repositories/post_test.go
+++ standalone/repositories/post_test.go
@@ -3,7 +3,6 @@ package repositories_test
 import (
 	"fmt"
 	"github.com/aacfactory/fns-contrib/databases/postgres"
-	_ "github.com/aacfactory/fns-contrib/databases/postgres/dialect"
 	"github.com/aacfactory/fns-example/standalone/repositories"
 	"github.com/aacfactory/fns/services/authorizations"
 	"github.com/aacfactory/fns/tests"
